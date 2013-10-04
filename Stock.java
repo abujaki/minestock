b9892ee0078ddaf3e8c093f4e28762ee6f969ec7
abujaki21 <abujaki21@gmail.com>
2013-10-04 17:00:55 -0400

    Create Stock.java
    Initial Commit

---
+++ Stock.java
@@ -0,0 +1,14 @@
+package io.github.abujaki.minestock;
+
+//Stock Object holds basic Stock information
+
+public class Stock {
+	private String code,fn, pl;
+	//Each stock has a stock code, a friendly name, and a player who is responsible for its creation
+	Stock(String stockCode, String friendlyName, String player){
+		code = stockCode; fn = friendlyName; pl = player;
+	}
+	String getCode(){return code;}
+	String getPlayer(){return pl;}
+	String getFriendlyName(){return fn;}
+}
