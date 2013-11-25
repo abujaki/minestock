6fe97f7ec8433f459311ba7538bdd426c5b5c14d
Andrew <abujaki21@gmail.com>
2013-11-25 11:24:59 -0500

    Making it prettier

--- src/io/github/abujaki/minestock/MineStock.java
+++ src/io/github/abujaki/minestock/MineStock.java
@@ -14,6 +14,7 @@ import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.permission.Permission;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -28,8 +29,8 @@ public class MineStock extends JavaPlugin {
 	public static Chat chat = null; //May be removed entirely. Not important, and throws NPEs
 	//protected static MemoryCard memoryCard;
 	protected TransactionEngine transactionEngine = new TransactionEngine();
-
-
+	private ChatColor colError = ChatColor.RED, colRoutine = ChatColor.DARK_GREEN, colTest = ChatColor.DARK_AQUA;
+	
 	//Enabler and Disabler
 	@Override
 	public void onDisable() {
@@ -55,17 +56,17 @@ public class MineStock extends JavaPlugin {
 		if(command.getLabel().equalsIgnoreCase("stockbuy")){
 			//stockbuy stock amount priceeach
 			if(!(sender instanceof Player)){
-				sender.sendMessage("This command requires you to be a logged in player");
+				sender.sendMessage(colError + "This command requires you to be a logged in player");
 				return true;
 			}
 			else{
 				//Check to see if we have 3 arguments
 				if (args.length >= 4) {
-					sender.sendMessage("Too many arguments");
+					sender.sendMessage(colError + "Too many arguments");
 					return false;
 				} 
 				if (args.length <= 2) {
-					sender.sendMessage("Not enough arguments");
+					sender.sendMessage(colError + "Not enough arguments");
 					return false;
 				}
 				//Convert args 2 and 3 to integers
@@ -73,7 +74,7 @@ public class MineStock extends JavaPlugin {
 					int amt = Integer.parseInt(args[1]);
 					int price = Integer.parseInt(args[2]);    	    	
 					if ((amt <= 0)||(price <= 0)){ //Smartass check
-						sender.sendMessage("You cannot buy with negative values.");
+						sender.sendMessage(colError + "You cannot buy with negative values.");
 						return false;
 					}
 					else return buyStock(sender, args[0], amt, price);	//-----------Finally run the buystock code
@@ -85,17 +86,17 @@ public class MineStock extends JavaPlugin {
 		}else if(command.getLabel().equalsIgnoreCase("stocksell")){
 			//stockbuy stock amount priceeach
 			if(!(sender instanceof Player)){
-				sender.sendMessage("This command requires you to be a logged in player");
+				sender.sendMessage(colError + "This command requires you to be a logged in player");
 				return true;
 			}
 			else{
 				//Check to see if we have 3 arguments
 				if (args.length >= 4) {
-					sender.sendMessage("Too many arguments");
+					sender.sendMessage(colError + "Too many arguments");
 					return false;
 				} 
 				if (args.length <= 2) {
-					sender.sendMessage("Not enough arguments");
+					sender.sendMessage(colError + "Not enough arguments");
 					return false;
 				}
 				//Convert args 2 and 3 to integers
@@ -103,7 +104,7 @@ public class MineStock extends JavaPlugin {
 					int amt = Integer.parseInt(args[1]);
 					int price = Integer.parseInt(args[2]);    	    	
 					if ((amt <= 0)||(price <= 0)){ //Smartass check
-						sender.sendMessage("You cannot buy with negative values.");
+						sender.sendMessage(colError + "You cannot buy with negative values.");
 						return false;
 					}
 					else return sellStock(sender, args[0], amt, price); //-----------Finally run the sellstock code
@@ -117,34 +118,34 @@ public class MineStock extends JavaPlugin {
 
 	private boolean buyStock(CommandSender sender, String stock, int amount, int priceEach){
 		//function to place a stock order
-		sender.sendMessage("You wish to buy " + String.valueOf(amount) + " of " + stock + " stock at " + String.valueOf(priceEach) + ".");
+		sender.sendMessage(colRoutine + "You wish to buy " + String.valueOf(amount) + " of " + stock + " stock at " + String.valueOf(priceEach) + ".");
 		//If the user has enough money, deduct that much and place a stock order.
 		if(econ.getBalance(sender.getName()) >= (amount * priceEach)){
 			EconomyResponse r = econ.withdrawPlayer(sender.getName(), amount*priceEach);
 			//Copypasta from example code
 			if(r.transactionSuccess()) {
-				sender.sendMessage(String.format("You paid %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
+				sender.sendMessage(String.format(colRoutine + "You paid %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
 				transactionEngine.match(new StockOrder(stock, amount, priceEach, sender.getName()), true);
 			} else {
-				sender.sendMessage(String.format("An error occured: %s", r.errorMessage));
+				sender.sendMessage(String.format(colError + "An error occured: %s", r.errorMessage));
 			}
 		} else {
-			sender.sendMessage("You don't have enough money to make that order");
+			sender.sendMessage(colError + "You don't have enough money to make that order");
 		}
-		sender.sendMessage(String.format("You now have %s of %s stock", transactionEngine.checkStockAmount(sender.getName(), stock),stock));
+		sender.sendMessage(String.format(colTest + "You now have %s of %s stock", transactionEngine.checkStockAmount(sender.getName(), stock),stock));
 		return true;
 	}
 
 	private boolean sellStock(CommandSender sender, String stock, int amount, int priceEach){
 		//function to put stocks up for sale
-		sender.sendMessage("You wish to sell " + String.valueOf(amount) + " of " + stock + " stock at " + String.valueOf(priceEach) + ".");
+		sender.sendMessage(colRoutine + "You wish to sell " + String.valueOf(amount) + " of " + stock + " stock at " + String.valueOf(priceEach) + ".");
 		if(amount >= transactionEngine.checkStockAmount(sender.getName(), stock)){
 			transactionEngine.match(new StockOrder(stock, amount, priceEach, sender.getName()), false);
 		} else {
-			sender.sendMessage(String.format("You don't have enough stocks to sell %s", amount));
+			sender.sendMessage(String.format(colError + "You don't have enough stocks to sell %s", amount));
 			return false;
 		}
-		sender.sendMessage(String.format("You now have %s of %s stock", transactionEngine.checkStockAmount(sender.getName(), stock),stock));
+		sender.sendMessage(String.format(colTest + "You now have %s of %s stock", transactionEngine.checkStockAmount(sender.getName(), stock),stock));
 		return true;
 	}
 
